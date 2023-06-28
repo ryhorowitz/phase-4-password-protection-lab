a9927574c7bd58c8b3276397b78ab2237e019a3a
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-06-28 13:36:01 -0400

    almost

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :authorize, only: [:show]
+
+  def show
+    user = User.find(session[:user_id])
+
+    render json: user, status: :ok
+  end
+
+  def create
+    user = User.create(user_params)
+    # byebug
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_comfirmation)
+  end
+
+  def authorize
+    return render json: { error: "Not_authorized" }, status: :unauthorized unless session.include? :user_id
+  end
+end
